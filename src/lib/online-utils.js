059666d2b4c9ba44a237d7d5bc2ffb6c28d4139b
davidi_playtika <davidi@playtika.com>
2025-08-25 17:19:58 +0300

    Added extra logging for the error.

--- src/lib/online-utils.js
+++ src/lib/online-utils.js
@@ -25,7 +25,7 @@ export async function fetchGame(method, url, data) {
 
     if (response.status != 200) {
         // goto(`/error?statusCode=${response.status}&message=Unabble to ${method} resource ${url}`)
-        return { status: response.status }
+        return { status: response.status, ...responseObject }
     }
 
     return {...responseObject, status: response.status}
